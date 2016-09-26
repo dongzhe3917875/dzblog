b8050c23a2b2fff818ff998d1b6969e5aa41aac6
dongzhe <dongzhe@360.cn>
2016-09-26 20:09:37 +0800

    add map constructor

--- study/develop/project/select_simulate/select_simulate.js
+++ study/develop/project/select_simulate/select_simulate.js
@@ -18,29 +18,62 @@ function heightSlide(node, status) {
   node.style.height = height;
 }
 
-
+var select_map = new WeakMap();
 jQuery(document).ready(function($) {
   var show_select = ".show_select";
-  var select_all = ".select_all"
-  var imitate_select = $(".imitate_select");
+  var select_all = ".select_all";
   var show_item = $(".show_item");
-  var show_select_option = {
-    "noborder": false
-  }
-  var show_select_all = {
-    "hide_select": true
-  }
-  $(show_select).changeClass(show_select_option);
-  $(select_all).changeClass(show_select_all);
+  var imitate_select = $(".imitate_select");
+
+  // var show_select_option = {
+  //   "noborder": false
+  // }
+  // var show_select_all = {
+  //   "hide_select": true
+  // }
+  // $(show_select).each(function(index, value) {
+  //   select_map.set($(value)[0], {
+  //     "noborder": false
+  //   })
+  // })
+  // $(select_all).each(function(index, value) {
+  //   select_map.set($(value)[0], {
+  //     "hide_select": true
+  //   })
+  // })
+
+  console.log(select_map);
+  // $(select_all).changeClass(show_select_all);
   $(show_select).on("click", function(event) {
     event.stopPropagation();
     var target = $(this);
+    if (!select_map.has(target[0])) {
+      select_map.set($(target)[0], {
+        "noborder": false
+      })
+      target.changeClass(select_map.get(target[0]))
+    }
+
+
     var select_all = $(target).next();
     var select_all_node = select_all[0];
 
-    show_select_all["hide_select"] = !show_select_all["hide_select"];
-    heightSlide(select_all_node, !show_select_all["hide_select"])
-    show_select_option["noborder"] = !show_select_option["noborder"];
+    if (!select_map.has(select_all_node)) {
+      select_map.set(select_all_node, {
+        "hide_select": false
+      })
+
+      $(select_all_node).changeClass(select_map.get(select_all_node))
+    } else {
+      select_map.get(select_all_node)["hide_select"] = !select_map.get(
+        select_all_node)["hide_select"];
+    }
+
+
+    heightSlide(select_all_node, !select_map.get(select_all_node)[
+      "hide_select"])
+    select_map.get(target[0])["noborder"] = !select_map.get(target[0])[
+      "noborder"];
   })
 
   var slideUp = function() {
