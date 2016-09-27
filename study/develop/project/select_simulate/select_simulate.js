3262735e95e122b1ee2fc9bea4cac9013bebe712
dongzhe <dongzhe@360.cn>
2016-09-27 17:08:47 +0800

    select simulate is finished 1.0

--- study/develop/project/select_simulate/select_simulate.js
+++ study/develop/project/select_simulate/select_simulate.js
@@ -14,35 +14,20 @@ function heightSlide(node, status) {
             0) + (parseInt(oStyle.borderBottomWidth) || 0);
         }
       });
+    height = height - parseInt(1) + "px";
   }
   node.style.height = height;
 }
 
 var select_map = new WeakMap();
 jQuery(document).ready(function($) {
+  // $(".imitate_select").each(function(index, value) {
+  //   var $this = $(this);
+  //   $this.width($this.find(".select_all").outerWidth());
+  // })
   var show_select = ".show_select";
   var select_all = ".select_all";
-  var show_item = $(".show_item");
   var imitate_select = $(".imitate_select");
-
-  // var show_select_option = {
-  //   "noborder": false
-  // }
-  // var show_select_all = {
-  //   "hide_select": true
-  // }
-  // $(show_select).each(function(index, value) {
-  //   select_map.set($(value)[0], {
-  //     "noborder": false
-  //   })
-  // })
-  // $(select_all).each(function(index, value) {
-  //   select_map.set($(value)[0], {
-  //     "hide_select": true
-  //   })
-  // })
-
-  console.log(select_map);
   // $(select_all).changeClass(show_select_all);
   $(show_select).on("click", function(event) {
     event.stopPropagation();
@@ -55,8 +40,7 @@ jQuery(document).ready(function($) {
     }
 
 
-    var select_all = $(target).next();
-    var select_all_node = select_all[0];
+    var select_all_node = $(target).next()[0];
 
     if (!select_map.has(select_all_node)) {
       select_map.set(select_all_node, {
@@ -74,22 +58,45 @@ jQuery(document).ready(function($) {
       "hide_select"])
     select_map.get(target[0])["noborder"] = !select_map.get(target[0])[
       "noborder"];
+
+    var select_all_array = Array.prototype.slice.call(document.querySelectorAll(
+      select_all));
+    select_all_array.forEach(function(ele) {
+      if (ele != select_all_node) {
+        slideUp(ele);
+      }
+    })
   })
 
-  var slideUp = function() {
-    var select_all_node = $(select_all)[0];
-    show_select_all["hide_select"] = true;
-    show_select_option["noborder"] = false;
+  var slideUp = function(select_all_node) {
+    var show_select_node = select_all_node.previousSibling;
+    if (select_map.has(select_all_node)) {
+      select_map.get(select_all_node)["hide_select"] = true;
+
+    }
+
+    if (select_map.has(show_select_node)) {
+      select_map.get(show_select_node)["noborder"] = false
+    }
+
     heightSlide(select_all_node, false);
   }
 
   $(document).on("click", function(event) {
-    slideUp();
+    // 将所有的select all 收起来
+    var select_all_array = Array.prototype.slice.call(document.querySelectorAll(
+      select_all));
+    select_all_array.forEach(function(ele) {
+      slideUp(ele);
+    })
   })
   $(document).on("click", ".select_item", function(event) {
     event.stopPropagation();
     var select_value = $(event.target).text();
+    var show_item = $(this).parents(".imitate_select").find(
+      ".show_item");
+    var select_all_node = this.parentNode;
     show_item.text(select_value);
-    slideUp();
+    slideUp(select_all_node);
   })
 });
