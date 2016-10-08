ace2aa21dc5f26fdc0b8850fcdeacb0491993ef1
dongzhe <dongzhe@360.cn>
2016-10-08 14:49:37 +0800

    add proto select simulate

--- study/develop/project/select_simulate/select_simulate.js
+++ study/develop/project/select_simulate/select_simulate.js
@@ -1,6 +1,29 @@
 require("select_simulate.css");
+var changeClass = require('./protoChangeClass').default;
 require("./jquery.select.js");
+var proto_select_simulate = require("./protoSelect.js").proto_select_simulate;
+// jquery select demo
 var select_map = new WeakMap()
-$(".imitate_select").select({
+$(".imitate_select.jqselect").select({
   select_map: select_map
+})
+
+// proto changeClass demo
+var dom = document.querySelector(".changeClass");
+var button = document.querySelector(".toggle");
+
+var demo_options = {
+  "changedemo": true
+}
+changeClass.call(dom, demo_options);
+button.addEventListener("click", function(event) {
+  demo_options.changedemo = !demo_options.changedemo
+})
+
+
+// proto select demo
+var domSelect = document.querySelector(".protoSelect");
+var select_map1 = new WeakMap();
+proto_select_simulate.call(domSelect, {
+  select_map: select_map1
 })
